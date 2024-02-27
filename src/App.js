378c13a4ad6bb117d5924131f9da36acef3f970e
Mike James <mikejames386@gmail.com>
2024-02-26 23:18:03 -0600

    debounce working but not very DRY

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React, {useEffect, useState } from 'react';
 import './App.css';
 
-
-// API
-// List: https://api.github.com/users
-// Search: https://api.github.com/search/users?q=capgemini&per_page=5
-// Task 1: List
-// Task 2: Search
-//
-// User data example
-// [
-//    {
-//      "login": "mojombo",
-//      "html_url": "https://github.com/mojombo",
-//    }
-// ]
-
 function App() {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+
+  // const fetchUserResponse = async (url) => {
+  //   const response = await fetch(url);
+  //   const json = await response.json();
+  //   return json;
+  // }
   
   useEffect(() => {
-    let searchUrl = 'https://api.github.com/users';
+    const searchUrl = 'https://api.github.com/';
+    let params;
     if (searchTerm.length > 0) {
-      searchUrl = `https://api.github.com/search/users?q=${searchTerm}&per_page=5`;
-      fetch(searchUrl)
-        .then(response => response.json())
-        .then(data => setUsers(data.items));
+      params = `search/users?q=${searchTerm}&per_page=5`;
+      const getUserData = setTimeout(() => {
+        fetch(searchUrl + params)
+          .then(response => response.json())
+          .then(data => setUsers(data.items));
+      }, 250);
+      return () => clearTimeout(getUserData);
     } else {
-      fetch(searchUrl)
+      params = 'users';
+      fetch(searchUrl + params)
         .then(response => response.json())
         .then(data => setUsers(data));
     };
