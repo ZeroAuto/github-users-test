3868b4469e86f48aa5e546717bb869ae210c23e3
Mike James <mikejames386@gmail.com>
2024-02-27 18:00:27 -0600

    use tailwind so it looks much much nicer

--- src/App.js
+++ src/App.js
@@ -26,11 +26,38 @@ function App() {
   };
   
   return (
-    <main>
-      <h1>Git Hub User List</h1>
-      <input onChange={handleInputChange} />
-      {users.length > 0 && users.map((user) => <div key={user.id}><a href={user.html_url}>{user.login}</a></div>)}
-    </main>
+    <div className="bg-gray-900 text-white h-screen">
+      <div className="container mx-auto py-8">
+        <div className="max-w-md mx-auto bg-gray-800 p-5 rounded-lg shadow-md">
+          <div className="mb-4">
+            <input
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Search users..."
+              className="w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            />
+          </div>
+          
+          <ul>
+            {users && users.map((user) => {
+              return (
+                <li
+                  key={user.id}
+                  className="flex items-center py-2 border-b border-gray-700"
+                >
+                  <img
+                    alt="User"
+                    className="w-10 h-10 rounded-full mr-3"
+                    src={user.avatar_url}
+                  />
+                  <span className="text-gray-300">{user.login}</span>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
